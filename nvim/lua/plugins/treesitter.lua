109f1f7fcda2ac45e775508afc2c56a3a325b4fc
Wetar <azrilharis75@gmail.com>
2026-07-26 20:36:14 +0700

    update(neovim): make treesitter auto install needed parser

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,26 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter").setup({
-			install_dir = vim.fn.stdpath("data") .. "/site",
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "*" },
+				callback = function(args)
+					local ft = vim.bo[args.buf].filetype
+					local lang = vim.treesitter.language.get_lang(ft)
+
+					if not vim.treesitter.language.add(lang) then
+						local available = vim.g.ts_available or require("nvim-treesitter").get_available()
+						if not vim.g.ts_available then
+							vim.g.ts_available = available
+						end
+						if vim.tbl_contains(available, lang) then
+							require("nvim-treesitter").install(lang)
+						end
+					end
+					if vim.treesitter.language.add(lang) then
+						vim.treesitter.start(args.buf, lang)
+					end
+				end,
+			}),
 		})
 	end,
 }
